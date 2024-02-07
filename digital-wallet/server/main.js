9c7607440aa9af490eb287ab27005f590786689c
tapioquito <caioportoramos@gmail.com>
2024-02-07 08:18:06 -0300

    created and imported more collections

--- digital-wallet/server/main.js
+++ digital-wallet/server/main.js
@@ -3,6 +3,8 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/ContactsCollection';
 import '../imports/api/ContactsMethods';
 import '../imports/api/ContactsPublications';
+import '..imports/api/TransactionsCollection';
+import '..imports/api/WalletsCollection';
 
 Meteor.startup(async () => {});
 
